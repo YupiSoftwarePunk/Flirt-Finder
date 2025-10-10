fbd6aa24e61a6016f81193a295498287059458f9
Yupi <youngperchik2404@gmail.com>
2025-10-10 20:07:28 +0500

    fixed some bugs, need to create jwt token and connect back and front

--- Server/Program.cs
+++ Server/Program.cs
@@ -1,8 +1,11 @@
-
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Server.Data;
 using Server.Repositories;
 using Server.Repositories.Interfaces;
+using System.Security.Cryptography;
+using System.Text;
 
 namespace Server
 {
@@ -22,7 +25,26 @@ namespace Server
             builder.Services.AddDbContext<AppDbContext>(options =>
             options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
 
-            builder.Services.AddScoped<IUserRepository, UserRepository>();
+            //builder.Services.AddScoped<IUserRepository, UserRepository>();
+
+            builder.Services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+.AddJwtBearer(options =>
+{
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = builder.Configuration["Jwt:Issuer"],
+        ValidAudience = builder.Configuration["Jwt:Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
 
             var app = builder.Build();
@@ -33,6 +55,7 @@ namespace Server
                 app.MapOpenApi();
             }
 
+            app.UseAuthentication();
             app.UseHttpsRedirection();
 
             app.UseAuthorization();
