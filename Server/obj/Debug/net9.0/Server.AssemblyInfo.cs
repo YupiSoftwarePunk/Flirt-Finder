84b768f353bce51bd6d62f89f8e4bc8e40aca2ba
Yupi <youngperchik2404@gmail.com>
2026-05-23 12:00:00 +0500

    edit structure

--- Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
+++ Server/obj/Debug/net9.0/Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08e29ea37a437b4198288c5553397f1f66e5991d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6786b407b44fbfcdd5a12b4446c45822e175fd0e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
